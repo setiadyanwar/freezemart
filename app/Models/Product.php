eb946b12801dd8fa2256217d2062fb36a4712615
asdw <asdwdev@gmail.com>
2025-02-10 13:57:39 +0700

    benerin fitur search & filtering

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Product extends Model
 {
@@ -17,16 +18,46 @@ class Product extends Model
     }
 
     // relationship
-    public function category() : BelongsTo
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
 
-    public function carts() : HasMany
+    public function carts(): HasMany
     {
         return $this->hasMany(Cart::class);
     }
 
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        $query->when(
+            $filters['search'] ?? false,
+            fn($query, $search) =>
+            $query->where('name', 'like', '%' . $search . '%')
+        );
 
+        $query->when(
+            !empty($filters['categories']) && is_array($filters['categories']),
+            function ($query) use ($filters) {
+                $query->whereHas('category', function ($q) use ($filters) {
+                    $q->whereIn('slug', $filters['categories']);
+                });
+            }
+        );
 
+        $query->when(
+            $filters['sort_by'] ?? false,
+            function ($query, $sortBy) {
+                if ($sortBy === 'oldest') {
+                    $query->orderBy('created_at', 'asc');
+                } elseif ($sortBy === 'newest') {
+                    $query->orderBy('created_at', 'desc');
+                } elseif ($sortBy === 'cheapest') {
+                    $query->orderBy('price', 'asc');
+                } elseif ($sortBy === 'expensive') {
+                    $query->orderBy('price', 'desc');
+                }
+            }
+        );
+    }
 }
