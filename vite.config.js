961b21ad259e9a8db79c5f56ed56202d110dce02
setiadyanwar <setiadyanwar@gmail.com>
2025-02-28 11:02:35 +0700

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/costum.js'],
+            input: [
+                'resources/js/landing.js',
+                'resources/css/app.css',
+                'resources/js/app.js', 
+            ],
             refresh: true,
         }),
     ],
