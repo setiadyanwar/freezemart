82f992fa4767e3fdfee553993f9b13edbc29d9be
asdw <asdwdev@gmail.com>
2025-05-01 14:51:45 +0700

    hapus tombol love/like di fitur chat

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,10 +1,10 @@
 @extends('templates.master')
 
 @section('content')
-    <section class="py-8 mt-16 antialiased bg-white dark:bg-gray-900 md:py-16">
-        <div class="max-w-screen-xl px-4 mx-auto 2xl:px-0">
+    <section class="mt-16 bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
+        <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
             <div class="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
-                <div class="max-w-md mx-auto shrink-0 lg:max-w-lg">
+                <div class="mx-auto max-w-md shrink-0 lg:max-w-lg">
                     <img class="w-full rounded" src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" />
                 </div>
 
@@ -14,7 +14,7 @@
                         <p class="text-2xl font-extrabold text-gray-900 dark:text-white sm:text-3xl">Rp
                             {{ number_format($product->price, 0, ',', '.') }}</p>
 
-                        <div class="flex items-center gap-2 mt-2 sm:mt-0">
+                        <div class="mt-2 flex items-center gap-2 sm:mt-0">
                             <div class="flex items-center gap-1">
                                 @for ($i = 1; $i <= 5; $i++)
                                     <svg class="{{ $i <= round($average_rating) ? 'text-yellow-300' : 'text-gray-300' }} h-4 w-4"
@@ -22,9 +22,9 @@
                                         fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495
-                                                        2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992
-                                                        2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39
-                                                        3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                                                            2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992
+                                                            2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39
+                                                            3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
                                     </svg>
                                 @endfor
                             </div>
@@ -45,7 +45,7 @@
                             <button type="submit"
                                 class="mt-4 flex items-center justify-center rounded-lg bg-primary-500 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-600 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-500 dark:hover:bg-primary-600 dark:focus:ring-primary-500 sm:mt-0"
                                 role="button">
-                                <svg class="w-5 h-5 -ms-2 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                <svg class="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                     width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                         stroke-width="2"
@@ -64,57 +64,56 @@
         </div>
     </section>
 
-
     {{-- releated --}}
-    <section class="py-8 antialiased bg-gray-50 dark:bg-gray-900 md:py-12">
-        <div class="max-w-screen-xl px-4 pt-5 mx-auto 2xl:px-0">
+    <section class="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-12">
+        <div class="mx-auto max-w-screen-xl px-4 pt-5 2xl:px-0">
 
             <!-- Heading & Filters -->
-            <div class="items-end justify-start mt-8 mb-4 space-y-4 sm:flex sm:space-y-0 md:mb-8">
+            <div class="mb-4 mt-8 items-end justify-start space-y-4 sm:flex sm:space-y-0 md:mb-8">
                 <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Produk Terkait</h2>
             </div>
 
-            <div class="grid grid-cols-2 gap-4 mb-4 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
+            <div class="mb-4 grid grid-cols-2 gap-4 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
 
                 @foreach ($products as $product)
                     <div
-                        class="p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 dark:bg-gray-800">
-                        <div class="w-full h-56">
+                        class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+                        <div class="h-56 w-full">
                             <a href="/products/{{ $product->slug }}">
-                                <img class="h-full mx-auto rounded-t" src="{{ asset('storage/' . $product->image) }}"
+                                <img class="mx-auto h-full rounded-t" src="{{ asset('storage/' . $product->image) }}"
                                     alt="{{ $product->name }}" />
                             </a>
                         </div>
                         <div class="pt-6">
                             <a href="/products/{{ $product->slug }}"
                                 class="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">{{ $product->name }}</a>
-                            <div class="flex items-center gap-2 mt-2">
+                            <div class="mt-2 flex items-center gap-2">
                                 <div class="flex items-center">
-                                    <svg class="w-4 h-4 text-yellow-400" aria-hidden="true"
+                                    <svg class="h-4 w-4 text-yellow-400" aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
                                     </svg>
 
-                                    <svg class="w-4 h-4 text-yellow-400" aria-hidden="true"
+                                    <svg class="h-4 w-4 text-yellow-400" aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
                                     </svg>
 
-                                    <svg class="w-4 h-4 text-yellow-400" aria-hidden="true"
+                                    <svg class="h-4 w-4 text-yellow-400" aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
                                     </svg>
 
-                                    <svg class="w-4 h-4 text-yellow-400" aria-hidden="true"
+                                    <svg class="h-4 w-4 text-yellow-400" aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
                                     </svg>
 
-                                    <svg class="w-4 h-4 text-yellow-400" aria-hidden="true"
+                                    <svg class="h-4 w-4 text-yellow-400" aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
@@ -125,7 +124,7 @@
                                 <p class="text-sm font-medium text-gray-500 dark:text-gray-400">(455)</p>
                             </div>
 
-                            <div class="flex items-center justify-between gap-4 mt-4">
+                            <div class="mt-4 flex items-center justify-between gap-4">
                                 <p class="text-2xl font-extrabold leading-tight text-gray-900 dark:text-white">Rp
                                     {{ number_format($product->price, 0, ',', '.') }}</p>
 
@@ -133,7 +132,7 @@
                                     @csrf
                                     <button type="submit"
                                         class="inline-flex items-center rounded-lg bg-primary-500 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-600 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-500 dark:hover:bg-primary-600 dark:focus:ring-primary-800">
-                                        <svg class="w-5 h-5 me-0 ms-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        <svg class="me-0 ms-0 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                             width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                                 stroke-width="2"
@@ -153,10 +152,10 @@
     {{-- TOAST --}}
     @if (session('success'))
         <div id="toast-bottom-right"
-            class="fixed flex items-center w-auto max-w-xs p-4 space-x-3 text-green-500 transition-all duration-1000 ease-in-out transform translate-x-0 border-2 border-green-300 rounded-lg bottom-5 right-5 bg-green-50 dark:border-green-800 dark:bg-gray-800 dark:text-green-400"
+            class="fixed bottom-5 right-5 flex w-auto max-w-xs translate-x-0 transform items-center space-x-3 rounded-lg border-2 border-green-300 bg-green-50 p-4 text-green-500 transition-all duration-1000 ease-in-out dark:border-green-800 dark:bg-gray-800 dark:text-green-400"
             role="alert">
-            <div class="flex items-center justify-center w-8 h-8 p-2 bg-green-100 rounded-full dark:bg-green-900">
-                <svg aria-hidden="true" class="w-6 h-6 text-green-500 dark:text-green-400" fill="currentColor"
+            <div class="flex h-8 w-8 items-center justify-center rounded-full bg-green-100 p-2 dark:bg-green-900">
+                <svg aria-hidden="true" class="h-6 w-6 text-green-500 dark:text-green-400" fill="currentColor"
                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
                         d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
